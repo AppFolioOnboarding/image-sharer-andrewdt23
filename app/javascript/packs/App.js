82946d04bff4508259a46c3af142de040275f4bd
Andrew Thompson <andrew.thompson@appfolio.com>
2019-06-18 13:10:37 -0700

    adt-use react to render content instead of rails

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello, React!</h1>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('App')
+);
+
+export default App;
