38617eb37b76f92c0a7663403485f05f5a6ee7cf
Andrew Thompson <andrew.thompson@appfolio.com>
2019-06-19 17:47:49 -0700

    adt - do not allow the backend to accept invalid urls
    adt - remove auto added files
    
    adt - small img validation refactor

--- app/controllers/api/image_url_controller.rb
+++ app/controllers/api/image_url_controller.rb
@@ -1,11 +1,24 @@
+require 'uri'
+
 class Api::ImageUrlController < Api::BaseController
+
+  def valid_url?(url)
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
 
   def index
     respond_with ImageUrl.order(created_at: :desc)
   end
 
   def create
-    respond_with :api, ImageUrl.create(imageUrl_params)
+    if is_valid_url?(imageUrl_params["url"]) && (['.jpeg', '.jpg', '.gif', '.png'].any? { |img| imageUrl_params["url"].include? img})
+      respond_with :api, ImageUrl.create(imageUrl_params)
+    else
+      head :bad_request
+    end
   end
 
   def destroy
